0ab12602c047d46c6867c4ba1822c4b47afc423f
Peyton Casper <peytoncas@gmail.com>
2020-04-15 23:25:37 -0400

    Initial commit

---
+++ load.sh
@@ -0,0 +1,33 @@
+if [ "$1" == "" ]; then
+    echo "Specify the type of value to load. (alphabet or template)"
+    exit 1
+fi
+
+if [ "$2" == "" ]; then
+    echo "Specify the required template or alphabet to load into Vault."
+    exit 1
+fi
+
+if [ "$3" == "" ]; then
+    echo "Specify the name that will be used in Vault to save this template or alphabet"
+    exit 1
+fi
+
+if [ "$1" == "template" ] && [ "$4" == "" ]; then
+    echo "Loading a template requires an alphabet to be specified"
+    exit 1
+fi
+
+TYPE=$1
+PATH_TO_LOAD=$2
+NAME=$3
+TEMPLATE_ALPHABET=$4
+
+if [ $TYPE == "alphabet" ]; then
+    vault write transform/alphabet/one-to-nine-numeric alphabet="$(cat $PATH_TO_LOAD)"
+elif [ $TYPE == "template" ]; then
+    vault write transform/template/$NAME \
+        type=regex \
+        pattern="$(cat $PATH_TO_LOAD)" \
+        alphabet=$TEMPLATE_ALPHABET
+fi
